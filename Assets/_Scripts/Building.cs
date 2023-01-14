37f3371aec41caa0d0c2d48f6ba84f9acbcd8b6c
Ege Altıok <egealtiok@gmail.com>
2023-01-15 01:31:41 +0300

    Added building upgrade and connect functionalities

--- Assets/_Scripts/Building.cs
+++ Assets/_Scripts/Building.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -6,34 +7,161 @@ namespace _Scripts
 {
     public class Building : MonoBehaviour
     {
-        protected static Building selectedBuilding;
-        protected int nextUpdate;
-        
+        public static Building selectedBuilding;
+        public static bool isConnecting;
+
+        public static readonly int[] BuildingCosts = {20, 30, 50};
+
         public int level;
+        public BuildingType buildingType;
+        public Color buildingColor;
+
+        protected int nextUpdate;
         public Building inputConnection;
         public List<Building> outputConnections;
         public int maxConnections;
-        
-        public TextMeshPro buildingText;
 
-        public Building()
+        public TextMeshPro floatingBuildingText;
+        public BuildingPopupScript buildingPopup;
+
+        protected Building()
         {
             nextUpdate = 1;
-            level = 0;
+            level = 1;
             maxConnections = 0;
             inputConnection = null;
             outputConnections = new List<Building>();
         }
 
+        public virtual void Upgrade()
+        {
+        }
+
+        public virtual int GetUpgradedOutput()
+        {
+            return 0;
+        }
+
+        public void BaseUpgrade()
+        {
+            level += 1;
+            UpdateBuildingText();
+            buildingPopup.UpdateElementValues();
+        }
+
+        public void ConnectOutput()
+        {
+            if (inputConnection)
+            {
+                Debug.Log("This building already has a connection.");
+                isConnecting = false;
+                return;
+            }
+
+            BuildingType? targetBuildingType = selectedBuilding.buildingType switch
+            {
+                BuildingType.Industrial => BuildingType.Commercial,
+                BuildingType.Commercial => BuildingType.Residential,
+                BuildingType.Residential => null,
+                _ => null
+            };
+            if (targetBuildingType != buildingType) // Invalid connection
+            {
+                Debug.Log("This building already has a connection.");
+                isConnecting = false;
+                return;
+            }
+
+            inputConnection = selectedBuilding;
+            floatingBuildingText.faceColor = buildingColor;
+            selectedBuilding.outputConnections.Add(this);
+            selectedBuilding.floatingBuildingText.outlineColor = Color.black;
+            selectedBuilding.buildingPopup.UpdateElementValues();
+
+            GameObject connectionLine = new GameObject();
+
+            LineRenderer lineRenderer = connectionLine.AddComponent<LineRenderer>();
+            lineRenderer.startWidth = lineRenderer.endWidth = 0.3f;
+
+            Vector3 sourceBuildingPosition = selectedBuilding.gameObject.transform.position;
+            Vector3 targetBuildingPosition = gameObject.transform.position;
+            lineRenderer.SetPositions(new[] {sourceBuildingPosition, targetBuildingPosition});
+
+            lineRenderer.colorGradient = new Gradient
+            {
+                colorKeys = new[]
+                {
+                    new GradientColorKey(selectedBuilding.buildingColor, 0.0f),
+                    new GradientColorKey(buildingColor, 1.0f)
+                }
+            };
+
+            Shader defaultShader = Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply");
+            lineRenderer.material = new Material(defaultShader);
+
+            isConnecting = false;
+        }
+
+        protected void SelectBuilding()
+        {
+            if (isConnecting)
+            {
+                ConnectOutput();
+                return;
+            }
+
+            if (inputConnection || buildingType == BuildingType.Industrial)
+            {
+                if (!selectedBuilding) // Select
+                {
+                    buildingPopup.ShowPopupAction();
+                }
+                else if (selectedBuilding == this) // Deselect
+                {
+                    buildingPopup.HidePopupAction();
+                }
+                else if (selectedBuilding != this) // Switch selection
+                {
+                    selectedBuilding.buildingPopup.HidePopupAction();
+                    buildingPopup.ShowPopupAction();
+                }
+                else
+                {
+                    throw new Exception("Unexpected error.");
+                }
+            }
+        }
+
         protected void CreateBuildingText(Color textColor)
         {
             GameObject textGameObject = Instantiate(BuildingSystem.instance.buildingTextObject, gameObject.transform);
-            buildingText = textGameObject.GetComponent<TextMeshPro>();
-            buildingText.faceColor = textColor;
+            if (buildingType == BuildingType.Industrial)
+            {
+                textGameObject.transform.Translate(-1, 0, 0);
+            }
+
+            floatingBuildingText = textGameObject.GetComponent<TextMeshPro>();
+            floatingBuildingText.faceColor = textColor;
         }
+
+        protected void CreateBuildingPopup()
+        {
+            GameObject infoPopup = Instantiate(
+                BuildingUI.instance.baseBuildingPopupObject,
+                GameObject.Find("GameUI").transform);
+            buildingPopup = infoPopup.GetComponent<BuildingPopupScript>();
+            buildingPopup.building = this;
+        }
+
         public void UpdateBuildingText()
         {
-            buildingText.text = outputConnections.Count + "/" + maxConnections;
+            // floatingBuildingText.text = outputConnections.Count + "/" + maxConnections;
+            floatingBuildingText.text = "Level " + level;
+        }
+
+        public int GetUpgradeCost()
+        {
+            return BuildingCosts[(int) buildingType] * level * 2 / 3;
         }
     }
 
@@ -43,14 +171,19 @@ namespace _Scripts
 
         void Start()
         {
+            buildingType = BuildingType.Residential;
+            buildingColor = Color.green;
+            maxConnections = level;
             CreateBuildingText(Color.grey);
-            Upgrade();
+            CreateBuildingPopup();
         }
+
         void Update()
         {
             GenerateIncome();
+            floatingBuildingText.text = stockpile + "$";
         }
-        
+
         void OnMouseDown()
         {
             if (stockpile >= 0)
@@ -58,87 +191,64 @@ namespace _Scripts
                 CollectIncome();
             }
 
-            if (selectedBuilding is CommercialBuilding && !inputConnection)
-            {
-                inputConnection = selectedBuilding;
-                buildingText.faceColor = Color.green;
-                
-                selectedBuilding.outputConnections.Add(this);
-                selectedBuilding.UpdateBuildingText();
-                selectedBuilding.buildingText.outlineColor = Color.black;
-                selectedBuilding = null;
-            }
-            else if (this == selectedBuilding)
-            {
-                selectedBuilding = null;
-                buildingText.outlineColor = Color.black;
-            }
+            SelectBuilding();
         }
-        
-        public void Upgrade()
+
+        public override void Upgrade()
         {
-            level += 1;
-            maxConnections = level;
-            UpdateBuildingText();
+            BuildingSystem.money -= GetUpgradeCost();
+            maxConnections = GetUpgradedOutput();
+            BaseUpgrade();
         }
-        public void GenerateIncome()
+
+        public override int GetUpgradedOutput()
+        {
+            return level + 1;
+        }
+
+        private void GenerateIncome()
         {
             if (Time.time >= nextUpdate && inputConnection)
             {
                 nextUpdate = Mathf.FloorToInt(Time.time) + 1;
                 stockpile += level;
             }
-            buildingText.text = stockpile.ToString();
         }
 
         public void CollectIncome()
         {
-            BuildingSystem.instance.money += stockpile;
+            BuildingSystem.money += stockpile;
             stockpile = 0;
         }
-        
     }
 
     public class CommercialBuilding : Building
     {
         void Start()
         {
+            buildingType = BuildingType.Commercial;
+            buildingColor = Color.blue;
+            maxConnections = level * 2;
+
             CreateBuildingText(Color.grey);
-            Upgrade();
+            CreateBuildingPopup();
         }
 
         void OnMouseDown()
         {
-            if (selectedBuilding is null && inputConnection && outputConnections.Count < maxConnections)
-            {
-                buildingText.outlineColor = Color.red;
-                selectedBuilding = this;
-                return;
-            }
-
-            if (selectedBuilding is IndustrialBuilding && !inputConnection)
-            {
-                inputConnection = selectedBuilding;
-                buildingText.faceColor = Color.blue;
-                
-                selectedBuilding.outputConnections.Add(this);
-                selectedBuilding.UpdateBuildingText();
-                selectedBuilding.buildingText.outlineColor = Color.black;
-                selectedBuilding = null;
-                // isActive = true;
-            }
-            else if (this == selectedBuilding)
-            {
-                selectedBuilding = null;
-                buildingText.outlineColor = Color.black;
-            }
+            SelectBuilding();
         }
 
-        public void Upgrade()
+        public override void Upgrade()
         {
-            level += 1;
-            maxConnections = level * 2;
-            UpdateBuildingText();
+            BuildingSystem.money -= GetUpgradeCost();
+            maxConnections = GetUpgradedOutput();
+            BaseUpgrade();
+        }
+
+        public override int GetUpgradedOutput()
+        {
+            return (level + 1) * 2;
         }
     }
 
@@ -146,27 +256,36 @@ namespace _Scripts
     {
         void Start()
         {
-            CreateBuildingText(Color.yellow);
-            Upgrade();
+            buildingType = BuildingType.Industrial;
+            buildingColor = Color.yellow;
+            maxConnections = level + 1;
+
+            CreateBuildingText(buildingColor);
+            CreateBuildingPopup();
         }
+
         void OnMouseDown()
         {
-            if (selectedBuilding is null && outputConnections.Count < maxConnections)
-            {
-                selectedBuilding = this;
-                buildingText.outlineColor = Color.red;
-            }
-            else if (this == selectedBuilding)
-            {
-                selectedBuilding = null;
-                buildingText.outlineColor = Color.black;
-            }
+            SelectBuilding();
         }
-        public void Upgrade()
-        {  
-            level += 1;
-            maxConnections = level + 1;
-            UpdateBuildingText();
+
+        public override void Upgrade()
+        {
+            BuildingSystem.money -= GetUpgradeCost();
+            maxConnections = GetUpgradedOutput();
+            BaseUpgrade();
         }
+
+        public override int GetUpgradedOutput()
+        {
+            return level + 2;
+        }
+    }
+
+    public enum BuildingType
+    {
+        Residential = 0,
+        Commercial = 1,
+        Industrial = 2
     }
 }
